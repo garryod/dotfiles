7ded35a0ddddd494e03efc8c7459e16461fca7be
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2023-11-08 21:26:17 +0000

    Use lazyvim extras for rust, docker & markdown

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -11,9 +11,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     -- import/override with your plugins
     { import = "plugins" },
   },
