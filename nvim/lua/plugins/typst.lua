ea435767dd3836928a016283a8d3b25bcfc16e5c
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2024-09-21 17:13:16 +0100

    Add typst lsp & treesitter

---
+++ nvim/lua/plugins/typst.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        typst_lsp = {}
+      }
+    }
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typst" })
+      end
+    end
+  }
+}
