574aa3508b432a608900feb1032eaa57d538dd78
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2024-11-01 19:34:15 +0000

    Add leetcode.nvim

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      arg = "leetcode.nvim",
+      lang = "rust",
+    },
+  },
+}
