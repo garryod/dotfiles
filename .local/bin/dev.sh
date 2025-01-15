824ef8e8e6f1ced3093f86b50125474328086978
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2025-01-15 10:25:36 +0000

    Add common-utils to nvim devcontainer

--- .local/bin/dev.sh
+++ .local/bin/dev.sh
@@ -7,6 +7,10 @@ devcontainer up \
   --skip-post-attach \
   --additional-features '
     {
+      "ghcr.io/devcontainers/features/common-utils:2.4.2": {
+        "username": "none",
+        "upgradePackages": false
+      },
       "ghcr.io/duduribeiro/devcontainer-features/neovim:1.0.1": {},
       "ghcr.io/devcontainers-extra/features/fzf:1.0.14": {},
       "ghcr.io/GeorgOfenbeck/features/lazygit-linuxbinary:1.0.0": {}
